0bca436670b9e772e41d30386ac19c917fbfdc4d
Bill Sideris <bill88t@feline.gr>
2024-03-21 20:35:50 +0200

    Fix typo

--- files/uninstaller.py
+++ files/uninstaller.py
@@ -1,3 +1,3 @@
-be.based.rm("rm /lib/telnet_console.mpy /lib/adafruit_connection_manager.mpy /lib/adafruit_ntp.mpy /lib/adafruit_requests.mpy /bin/iwctl/headless.py /bin/iwctl/interactive.py /bin/iwctl/main.py /bin/ping.lja /bin/ping.py /bin/iwctl.lja /bin/telnet.lja /bin/telnet.py /bin/timesync.lja /bin/timesync.py")
+be.based.run("rm /lib/telnet_console.mpy /lib/adafruit_connection_manager.mpy /lib/adafruit_ntp.mpy /lib/adafruit_requests.mpy /bin/iwctl/headless.py /bin/iwctl/interactive.py /bin/iwctl/main.py /bin/ping.lja /bin/ping.py /bin/iwctl.lja /bin/telnet.lja /bin/telnet.py /bin/timesync.lja /bin/timesync.py")
 
 be.api.setvar("return", "0")
